a5dcff199e8556d92a4c48aa4b26848a037dea42
Elyageen <yagosalih452@gmail.com>
2023-09-20 14:51:39 -0700

    fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,12 +4,12 @@
  * @dest: input v
  * @src: input v
  * @n: input v
- 
  * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int t;
+	
 	t = 0;
 	while (t < n && src[t] != '\0')
 	{
