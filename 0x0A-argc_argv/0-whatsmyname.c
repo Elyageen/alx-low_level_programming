e85e9164e7571123453d99ece71c442446ac3d7d
root <yagosalih452@gmail.com>
2023-11-19 10:37:33 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  a program that prints its name, followed by a new line
+ *argc: int
+ *argv: list
+ *return; 0
+ */
+
+int man(int argc, char const *argv {})
+
+{
+	(void)argc;
+	printf("%s\n", argv {0});
+	return(0);
+	}
