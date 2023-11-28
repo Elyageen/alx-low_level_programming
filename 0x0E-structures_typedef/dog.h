78b2d8199a7530a577e7b819283d118311c39065
root <yagosalih452@gmail.com>
2023-11-28 00:36:13 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,36 +1,70 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
 
-	/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
+/**
+ * init_dog - initializes a dog structure
+ * @d: Pointer to the dog structure
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
-	/**
-	 * dog_t - typedef for struct dog
-	 */
-	typedef struct dog dog_t;
+/**
+ * print_dog - prints information about a dog
+ * @d: Pointer to the dog structure
+ */
+void print_dog(struct dog *d);
 
+/**
+ * new_dog - creates a new dog structure
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Return: Pointer to the new dog structure
+ */
+dog_t *new_dog(char *name, float age, char *owner);
 
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
+/**
+ * free_dog - frees memory allocated for a dog structure
+ * @d: Pointer to the dog structure
+ */
+void free_dog(dog_t *d);
 
+/**
+ * _strcpy - copies a string to a buffer
+ * @dest: Destination buffer
+ * @src: Source string
+ * Return: Pointer to the destination buffer
+ */
+char *_strcpy(char *dest, const char *src);
 
-	#endif
+/**
+ * _strlen - calculates the length of a string
+ * @s: Input string
+ * Return: Length of the string
+ */
+int _strlen(const char *s);
+
+#endif
 
